bc15375ecf63c2bc417fbe82e5626cf77a293641
AyoKayyo <mark@greenwoodstratton.com>
2025-07-01 11:41:02 -0400

    Update app.py

--- capture-one-preset-bot/app.py
+++ capture-one-preset-bot/app.py
@@ -50,10 +50,6 @@ def generate():
     build_costyle(settings, fname)
     return send_file(fname, as_attachment=True, mimetype='application/octet-stream')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3333, debug=True)
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3333))
     app.run(host="0.0.0.0", port=port, debug=True)
