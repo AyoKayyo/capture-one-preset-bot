bdfb55b5ecdbbb20727feb34d5ae975f2d378247
AyoKayyo <mark@greenwoodstratton.com>
2025-07-01 11:38:56 -0400

    Update app.py

--- capture-one-preset-bot/app.py
+++ capture-one-preset-bot/app.py
@@ -52,3 +52,8 @@ def generate():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3333, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3333))
+    app.run(host="0.0.0.0", port=port, debug=True)
